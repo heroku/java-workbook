fb5c67815cabcdc145a2dcaeb7720059e37a390b
James Ward <james@jamesward.org>
2011-07-21 18:32:14 -0600

    smaller, better, funner

--- tutorial-2/src/main/java/foo/TickDAO.java
+++ tutorial-2/src/main/java/foo/TickDAO.java
@@ -1,43 +1,58 @@
 package foo;
-
+   
 import java.sql.*;
-
+      
 public class TickDAO {
 
-    private Connection getConn() throws SQLException {
-        String dbUrl = System.getenv("DATABASE_URL");
-        dbUrl = dbUrl.replaceAll("postgres://(.*):(.*)@(.*)", "jdbc:postgresql://$3?user=$1&password=$2");
-        return DriverManager.getConnection(dbUrl);
-    }
+    static String dbUrl;
 
-    private void createTable() {
-        try {
-            Statement stmt = getConn().createStatement();
-            stmt.executeUpdate("CREATE TABLE ticks (tick timestamp)");
+    public TickDAO() {
+        if (dbUrl == null) {
+            dbUrl = System.getenv("DATABASE_URL").replaceAll("postgres://(.*):(.*)@(.*)", "jdbc:postgresql://$3?user=$1&password=$2");
+            dbUpdate("CREATE TABLE ticks (tick timestamp)");
         }
-        catch (SQLException e) {
-        }
-    }
-
-    public int getTickCount() {
-        try {
-            Statement stmt = getConn().createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT count(*) FROM ticks");
-            rs.next();
-            return rs.getInt(1);
-           }
-        catch (SQLException e) {
-        }
-        return -1;
     }
 
     public void insertTick() {
+        dbUpdate("INSERT INTO ticks VALUES (now())");
+    }
+
+    private void dbUpdate(String sql) {
+        Connection dbConn = null;
         try {
-            createTable();
-            Statement stmt = getConn().createStatement();
-            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            dbConn = DriverManager.getConnection(dbUrl);
+            Statement stmt = dbConn.createStatement();
+            stmt.executeUpdate(sql);
         }
         catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                dbConn.close();
+            }
+            catch (SQLException ignore) {
+            }
         }
     }
+
+    public int getTickCount() throws SQLException {
+        Connection dbConn = null;
+        try {
+            dbConn = DriverManager.getConnection(dbUrl);
+            Statement stmt = dbConn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT count(*) FROM ticks");
+            rs.next();
+            return rs.getInt(1);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            dbConn.close();
+        }
+
+        return -1;
+    }
+ 
 }
